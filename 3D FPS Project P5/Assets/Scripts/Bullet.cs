61d63e2e2708f483671a95c245bc55a6f631d52f
Justin Scott <hello@jrs.dev>
2024-03-15 14:15:50 -0700

    adding targets

--- 3D FPS Project P5/Assets/Scripts/Bullet.cs
+++ 3D FPS Project P5/Assets/Scripts/Bullet.cs
@@ -19,4 +19,24 @@ public class Bullet : MonoBehaviour
     {
         _rigidbody.velocity = transform.forward * moveSpeed;
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("TargetStanding"))
+        {
+            Debug.Log("I hit the standing target.");
+            //add code to add hit points to your scoreboard
+            //gray out the standing target
+            Destroy(other.gameObject);
+            other.gameObject.SetActive(false);
+        }
+
+        if(other.gameObject.CompareTag("TargetFloating"))
+        {
+            Debug.Log("I hit the floating target");
+            //add code to add hit points to your scoreboard
+            Destroy(this.gameObject);
+            Destroy(other.gameObject);
+        }
+    }
 }
--- 3D FPS Project P5/Assets/Scripts/PlayerController.cs
+++ 3D FPS Project P5/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 10f;
     public float gravityModifier = 1f;
     public float mouseSensitivity = 1f;
+    public GameObject bullet;
+    public Transform firePoint;
     public Transform theCamera;
     public Transform groundCheckpoint;
     public LayerMask whatIsGround;
@@ -65,6 +67,27 @@ public class PlayerController : MonoBehaviour
         transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z);
 
         theCamera.rotation = Quaternion.Euler(theCamera.rotation.eulerAngles + new Vector3(-mouseInput.y, 0f, 0f));
+
+        //Handle Shooting
+        if(Input.GetMouseButtonDown(0))
+        {
+            //Find the crosshair
+            RaycastHit hit;
+            if(Physics.Raycast(theCamera.position, theCamera.forward, out hit, 50f))
+            {
+                if(Vector3.Distance(theCamera.position, hit.point) > 2f)
+                {
+                    firePoint.LookAt(hit.point);
+                }
+            }
+            else
+            {
+                firePoint.LookAt(theCamera.position + (theCamera.forward * 30f));
+            }
+
+            //Create the bullet
+            Instantiate(bullet, firePoint.position, firePoint.rotation);
+        }
     }
 }
  
